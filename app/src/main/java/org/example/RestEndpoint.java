77e08dde81bd785f07b0cc0680315a578bf7b781
gab <gab@MacBook-Pro.lan>
2026-05-12 20:25:27 +0200

    Initial commit

---
+++ app/src/main/java/org/example/RestEndpoint.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestEndpoint {
+
+    // Questo metodo risponde alle chiamate GET su /hello
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello, %s!", name);
+    }
+}
